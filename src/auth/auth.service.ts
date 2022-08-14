74fb06563574e08f95b1a1534ab8e069a7a75f8f
Shojibur <shojibur@gmail.com>
2022-08-14 11:03:15 +0800

    Signup service function complete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { AuthDto } from "./dto/auth.dto";
+import * as argon from "argon2";
+import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'hello' };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new ForbiddenException("User already exists");
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
-    return 'signin';
+    return "signin";
   }
 }
