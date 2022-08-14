95c5f6be5800bb50e9c6f1eb5ccefc17ca5227c8
Shojibur <shojibur@gmail.com>
2022-08-14 16:44:51 +0800

    Route guard concept

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,11 +4,15 @@ import { ConfigService } from "@nestjs/config";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
   constructor(private config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: config.get('JWT_SECRET'),
+      secretOrKey: config.get("JWT_SECRET"),
     });
+  }
+
+  validate(payload: any) {
+    return payload;
   }
 }
